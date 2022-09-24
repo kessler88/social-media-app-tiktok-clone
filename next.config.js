f1fe57a4cdc3d61fde278ae45230f2d3e2e5fd58
kessler88 <kesslerlock@gmail.com>
2022-09-24 19:33:09 -0400

    ...

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {ignoreBuildErrors: true},
+  typescript: { ignoreBuildErrors: true },
+  
+  
   reactStrictMode: true,
   images: {
     domains: ["media-exp1.licdn.com", "lh3.googleusercontent.com"], 
